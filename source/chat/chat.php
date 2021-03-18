db896e672e0283832c987c6c991b4528d74c6ed0
fmimmler <fmimmler@student.tgm.ac.at>
2021-03-18 15:03:28 +0100

    chatV0_1

--- source/chat/chat.php
+++ source/chat/chat.php
@@ -1,12 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat</title>
+
+    <link rel="stylesheet" href="style/styleChat.css">
 </head>
+
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <div class="col-11 ms-5">
+      <a class="navbar-brand nav-font" href="#">Chat</a>
+    </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ">
+        <a class="nav-link active" aria-current="page" href="#">Logout</a>
+      </div>
+    </div>
+  </div>
+</nav>
     
+
+<div class="heading">
+  <h1 class="display-3">
+    Chat
+  </h1>
+</div>
+<div id="chat">
+
+<p class="rightChat">text1links</p>
+<p class="rightChat">text1links</p>
+<p class="leftChat">text1links</p>
+<p class="rightChat">text1links</p>
+<p class="rightChat">text2rechts</p>
+<p class="rightChat">text2rechts</p>
+<p class="leftChat">text2rechts</p>
+<p class="rightChat">text2rechts</p>
+<p class="rightChat">text1links</p>
+<p class="rightChat">text1links</p>
+<p class="leftChat">text1links</p>
+<p class="rightChat">text1links</p>
+<p class="rightChat">text2rechts</p>
+<p class="rightChat">text2rechts</p>
+<p class="leftChat">text2rechts</p>
+
+
+
+</div>
+<div id="eingabe">
+
+<form action="" method="post">
+  <div class="d-flex flex-row">
+    <input class="col-10" type="text" name="text" id="text" placeholder="message">
+    <input class="col-2"type="submit" value="senden">
+  </div>
+</form>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
