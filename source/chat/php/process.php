e1d14a1336a6e8c95f878fbfe8aec2ec7140f6df
fmimmler <fmimmler@student.tgm.ac.at>
2021-04-06 10:44:50 +0200

    chat funktioniert mal so

--- source/chat/php/process.php
+++ source/chat/php/process.php
@@ -1,20 +1,21 @@
 <?php
-    $username = $email = $password = "";
+	SESSION_START();
+    $_SESSION["username"] = $_SESSION["email"] = $_SESSION["password"] = "";
     $userDataFile = '../datei/logindata.csv';
 
-    //Wird ausgeführt, wenn das Formular zur Registriereung abgeschickt wurde.
+    //Wird ausgefï¿½hrt, wenn das Formular zur Registriereung abgeschickt wurde.
     if(isset($_POST['submitRegister'])) {
 
-	//Informationen von der Registrierseite holen   
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = hash('sha256', $_POST['password']);
+		//Informationen von der Registrierseite holen   
+		$_SESSION["username"] = $_POST['username'];
+		$_SESSION["email"] = $_POST['email'];
+		$_SESSION["password"] = hash('sha256', $_POST['password']);
 
-	//Auf Sonderzeichen prüfen
-        $username = sonderzeichen($username);
-        $email = sonderzeichen($email);
+	//Auf Sonderzeichen prï¿½fen
+		$_SESSION["username"] = sonderzeichen($_SESSION["username"]);
+        $_SESSION["email"] = sonderzeichen($_SESSION["email"]);
 
-	//Daten aus dem File holen zur Überprüfung
+	//Daten aus dem File holen zur ï¿½berprï¿½fung
 	$handler = fopen($userDataFile, 'r');
 
 	    $usernames = array();
@@ -31,7 +32,7 @@
 		for ($c=1; $c < $num; $c+=3) {
                	    array_push($emails, $data[$c]);
             	}
-		if($usernames[$row] == $username or $emails[$row] == $email)	{
+		if($usernames[$row] == $_SESSION["username"] or $emails[$row] == $_SESSION["email"])	{
 		    $fail = 1;
 		}
 
@@ -46,21 +47,16 @@
 	    //Informationen in das File schreiben
 	    $handler = fopen($userDataFile, 'a+');
 
-	    	fwrite($handler, $username . ";" . $email . ";" . $password . "\n");
+	    	fwrite($handler, $_SESSION["username"] . ";" . $_SESSION["email"] . ";" . $_SESSION["password"] . "\n");
 	
 	    fclose($handler);
 	}
 	//Fehler-Meldung wird auf der registration-Site ausgegeben
 	else	{
-	    session_start();
             $_SESSION ['wrongregistration'] = "";
 	    header('Location: ../register.php');
 
 	}
-
-	
-	
-
 
 
 
@@ -75,7 +71,7 @@
 	//mail($empfaenger, $betreff, $text, $from);
     }
 
-    //Wird ausgeführt, wenn das Formular zur Registriereung abgeschickt wurde.
+    //Wird ausgefï¿½hrt, wenn das Formular zur Registriereung abgeschickt wurde.
     if(isset($_POST['submitLogin']))    {
 
     }
@@ -94,5 +90,26 @@
         return $string;
     }
 
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Importiert das Bootstrap css-File-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!--Importiert das css-File-->
+    <link rel="stylesheet" href="style/styleRegister.css">
+
+    <!--Importiert das Bootstrap script-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" async></script>
+    
+
+    <title>process</title>
+</head>
+<body>
+	<a href="../chat.php">Weiter zum Chat</a>
+</body>
+</html>
